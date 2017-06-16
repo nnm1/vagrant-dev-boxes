0c9b86c34ebd1af5d9f4dbd0dded38becf99c97a
skanukov <s.kanukov@gmail.com>
2017-06-16 12:22:04 +0300

    Add dropping db and user pg helper

--- postgres-box/helpers/postgres-create-help.sh
+++ postgres-box/helpers/postgres-create-help.sh
@@ -2,12 +2,12 @@
 # Print commands to create PostgreSQL db, user and to grant privileges.
 
 echo 'Create PostgreSQL database:'
-echo 'postgres=# CREATE DATABASE myproject;'
+echo 'postgres=# CREATE DATABASE db_name;'
 
 echo 'Create PostgreSQL user:'
-echo "postgres=# CREATE USER myproject WITH PASSWORD 'password';"
+echo "postgres=# CREATE USER user_name WITH PASSWORD 'password';"
 
 echo 'Grant privileges:'
-echo "postgres=# GRANT ALL PRIVILEGES ON DATABASE myproject TO myproject;"
+echo "postgres=# GRANT ALL PRIVILEGES ON DATABASE db_name TO user_name;"
 
 sudo -u postgres psql
---
+++ postgres-box/helpers/postgres-drop-help.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Print commands to drop PostgreSQL db and user.
+
+echo 'Drop PostgreSQL database:'
+echo 'postgres=# DROP DATABASE IF EXISTS db_name;'
+echo ''
+
+echo 'Drop PostgreSQL user:'
+echo "postgres=# DROP USER IF EXISTS user_name"
+echo ''
+
+echo 'Exit psql:'
+echo 'postgres=# \q'
+echo ''
