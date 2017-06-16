c8524ed402901c41ae87407988e924b717516dea
skanukov <s.kanukov@gmail.com>
2017-06-16 12:43:41 +0300

    Add php-dev-box

---
+++ php-maria-box/README.md
@@ -0,0 +1,19 @@
+# php-postgres-box
+
+A Vagrant-powered virtual machine for PHP application development.
+
+Default MariaDB port 3306 in the host computer is forwarded to port 3306 in the virtual machine.
+
+Port 8000 in the host computer is also forwarded to port 8000 in the virtual machine. Thus, applications running in the virtual machine can be accessed via localhost:8000 in the host computer. Be sure the web server is bound to the IP 0.0.0.0, instead of 127.0.0.1, so it can access all interfaces, e.g.:
+
+    $ php artisan serve --host=0.0.0.0
+
+## What's In The Box
+
+* MariaDB 10.2 with 'root:vagrant' superuser
+
+* PHP7 7.1 with the latest stable Composer
+
+* NodeJS 6.x LTS with Npm and the latest stable Yarn
+
+* The latest stable Git
---
+++ php-maria-box/bootstrap.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+# Bootstrap file for setting PHP development environment.
+
+# Enable non-interactive mode.
+export DEBIAN_FRONTEND=noninteractive \
+
+    # Set software versions.
+    PHP_VERSION='7.1' \
+    MARIADB_VERSION='10.2' \
+    NODE_VERSION='6.x'
+
+# Set locale and timezone.
+sudo locale-gen ru_RU.UTF-8 \
+&& sudo update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 \
+&& sudo timedatectl set-timezone Europe/Moscow
+
+# Install dependencies.
+sudo apt-get update -qq \
+&& sudo apt-get install -y --no-install-recommends \
+    software-properties-common \
+    wget
+
+# Install Git.
+sudo add-apt-repository ppa:git-core/ppa \
+&& sudo apt-get update -qq \
+&& sudo apt-get install -y --no-install-recommends git
+
+# Install PHP.
+sudo add-apt-repository ppa:ondrej/php \
+&& sudo apt-get update -qq \
+&& sudo apt-get install -y --no-install-recommends \
+    php"$PHP_VERSION" \
+    php"$PHP_VERSION"-mbstring \
+    php"$PHP_VERSION"-mysql \
+    php"$PHP_VERSION"-xml \
+    php"$PHP_VERSION"-zip # Laravel dependencies.
+
+# Install Composer.
+wget -qO- https://getcomposer.org/installer | \
+    sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+# Set MariaDB root password.
+sudo debconf-set-selections <<< \
+    'mysql-server mysql-server/root_password password vagrant' \
+&& sudo debconf-set-selections <<< \
+    'mysql-server mysql-server/root_password_again password vagrant'
+
+# Install MariaDB.
+sudo apt-key adv \
+    --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8 \
+&& sudo add-apt-repository \
+    "deb [arch=amd64,i386,ppc64el] http://mirror.mephi.ru/mariadb/repo/$MARIADB_VERSION/ubuntu xenial main" \
+&& sudo apt-get update -qq \
+&& sudo apt-get install -y --no-install-recommends mariadb-server
+
+# Install NodeJS and set Npm permissions.
+wget -qO- https://deb.nodesource.com/setup_"$NODE_VERSION" | sudo bash - \
+&& sudo apt-get install -y --no-install-recommends nodejs \
+&& mkdir ~/.npm-global \
+&& npm config set prefix '~/.npm-global' \
+&& echo 'export PATH=~/.npm-global/bin:$PATH' | tee -a ~/.profile \
+&& source ~/.profile
+
+# Install Yarn.
+npm install --global yarn
+
+echo 'All set, rock on!'
