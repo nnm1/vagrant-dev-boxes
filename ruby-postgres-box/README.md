91aeb00a7d5b016b6a050e6b7d79604f620ab96a
skanukov <s.kanukov@gmail.com>
2017-06-16 11:39:38 +0300

    Add ruby-postgres-box

---
+++ ruby-postgres-box/README.md
@@ -0,0 +1,19 @@
+# ruby-postgres-box
+
+A Vagrant-powered virtual machine for Ruby application development.
+
+Default PostgreSQL port 5432 in the host computer is forwarded to port 5432 in the virtual machine.
+
+Port 3000 in the host computer is also forwarded to port 3000 in the virtual machine. Thus, applications running in the virtual machine can be accessed via localhost:8000 in the host computer. Be sure the web server is bound to the IP 0.0.0.0, instead of 127.0.0.1, so it can access all interfaces, e.g.:
+
+    $ bin/rails server -b 0.0.0.0
+
+## What's In The Box
+
+* PostgreSQL 9.6 with 'postgres:vagrant' superuser
+
+* Ruby 2.4 with disabled automatic documentation
+
+* NodeJS 6.x LTS with Npm and the latest stable Yarn
+
+* The latest stable Git
---
+++ ruby-postgres-box/bootstrap.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+# Bootstrap file for setting Ruby development environment.
+
+# Enable non-interactive mode.
+export DEBIAN_FRONTEND=noninteractive \
+
+    # Set software versions.
+    RUBY_VERSION='2.4.1' \
+    POSTGRESQL_VERSION='9.6' \
+    NODE_VERSION='6.x'
+
+# Set locale and timezone.
+sudo locale-gen ru_RU.UTF-8 \
+&& sudo update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 \
+&& sudo timedatectl set-timezone Europe/Moscow
+
+# Install dependencies.
+sudo apt-get update -qq \
+&& sudo apt-get install -y --no-install-recommends \
+    build-essential \
+    software-properties-common \
+    wget
+
+# Install Git.
+sudo add-apt-repository ppa:git-core/ppa \
+&& sudo apt-get update -qq \
+&& sudo apt-get install -y --no-install-recommends git
+
+# Install rbenv.
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
+&& echo 'export PATH="$HOME/.rbenv/bin:$PATH"' | tee -a ~/.profile \
+&& echo 'eval "$(rbenv init -)"' | tee -a ~/.profile \
+&& source ~/.profile
+
+# Install ruby-build.
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build \
+&& echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' | \
+    tee -a ~/.profile \
+&& source ~/.profile
+
+# Install ruby.
+rbenv install "$RUBY_VERSION" \
+&& rbenv global "$RUBY_VERSION"
+
+# Disable ruby docs.
+echo 'gem: --no-document' | tee -a ~/.gemrc
+
+# Install Bundler.
+gem install bundler \
+&& rbenv rehash
+
+# Install PostgreSQL.
+echo 'deb https://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' | \
+    sudo tee -a /etc/apt/sources.list.d/pgdg.list \
+&& wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+    sudo apt-key add - \
+&& sudo apt-get update -qq \
+&& sudo apt-get install -y --no-install-recommends \
+    postgresql-"$POSTGRESQL_VERSION" \
+    postgresql-contrib-"$POSTGRESQL_VERSION" \
+    libpq-dev
+
+# Set 'postgres' user password.
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'vagrant';"
+
+# Allow external connections for PostgreSQL.
+echo 'host all all all md5' | \
+    sudo tee -a /etc/postgresql/"$POSTGRESQL_VERSION"/main/pg_hba.conf \
+&& sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" \
+    /etc/postgresql/"$POSTGRESQL_VERSION"/main/postgresql.conf
+
+# Restart the PostgreSQL server for the changes to take effect.
+sudo service postgresql restart
+
+# Install NodeJS and set Npm permissions.
+wget -qO- https://deb.nodesource.com/setup_"$NODE_VERSION" | sudo bash - \
+&& sudo apt-get install -y --no-install-recommends nodejs \
+&& mkdir ~/.npm-global \
+&& npm config set prefix '~/.npm-global' \
+&& echo 'export PATH=~/.npm-global/bin:$PATH' | tee -a ~/.profile \
+&& source ~/.profile
+
+# Install Yarn.
+npm install --global yarn
+
+echo 'All set, rock on!'
