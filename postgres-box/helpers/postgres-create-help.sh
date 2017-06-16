7c9684d838135c1816f79aa78d8c8e6b6749b254
skanukov <s.kanukov@gmail.com>
2017-06-16 12:24:36 +0300

    Prettify create helper out a bit

--- postgres-box/helpers/postgres-create-help.sh
+++ postgres-box/helpers/postgres-create-help.sh
@@ -3,11 +3,14 @@
 
 echo 'Create PostgreSQL database:'
 echo 'postgres=# CREATE DATABASE db_name;'
+echo ''
 
 echo 'Create PostgreSQL user:'
 echo "postgres=# CREATE USER user_name WITH PASSWORD 'password';"
+echo ''
 
 echo 'Grant privileges:'
 echo "postgres=# GRANT ALL PRIVILEGES ON DATABASE db_name TO user_name;"
+echo ''
 
 sudo -u postgres psql
