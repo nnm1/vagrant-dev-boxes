9384c16fc28a7d3fba16a2ea970c146dcb08bd6d
skanukov <s.kanukov@gmail.com>
2017-12-20 17:03:47 +0300

    Update help

--- mongo-box/create-db-owner-help.sh
+++ mongo-box/create-db-owner-help.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Prints commands to create a Mongo db owner.
 
-echo "Create 'test:test' db owner for the 'test' database:"
-echo $"mongo test --authenticationDatabase \"admin\" -u \"vagrant\" -p \"vagrant\" --eval \"db.createUser({ user: 'test', pwd: 'test', roles: [{ role: 'dbOwner', db: 'test' }] })\""
+echo 'Create "test:test" db owner for the "test" database:'
+echo 'mongo test --authenticationDatabase "admin" -u "vagrant" -p "vagrant" --eval $"db.createUser({ user: \"test\", pwd: \"test\", roles: [{ role: \"dbOwner\", db: \"test\" }] })"'
--- mongo-box/mongo-run-help.sh
+++ mongo-box/mongo-run-help.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Prints commands to run a Mongo shell.
 
-echo "Run a mongo shell for the 'test' db and 'test:test' user:"
+echo 'Run a mongo shell for the "test" db and "test:test" user:'
 echo 'mongo test --authenticationDatabase "test" -u "test" -p'
